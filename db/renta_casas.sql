76a7510988498af45bd5a0afb2d36f032ce329d6
laortizt <adri26.ortiz@gmail.com>
2021-04-12 15:25:50 -0500

    database

---
+++ db/renta_casas.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-04-2021 a las 16:56:24
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `renta_casas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dates`
+--
+
+CREATE TABLE `dates` (
+  `id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `finish_date` date NOT NULL,
+  `id_house` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `houses`
+--
+
+CREATE TABLE `houses` (
+  `name` varchar(150) NOT NULL,
+  `description` varchar(150) NOT NULL,
+  `rooms` int(11) NOT NULL,
+  `bath` int(11) NOT NULL,
+  `parking` tinyint(1) NOT NULL,
+  `internet` tinyint(1) NOT NULL,
+  `services` varchar(300) NOT NULL,
+  `location` varchar(150) NOT NULL,
+  `price` int(11) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `img_main` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `houses`
+--
+
+INSERT INTO `houses` (`name`, `description`, `rooms`, `bath`, `parking`, `internet`, `services`, `location`, `price`, `capacity`, `id`, `user_id`, `img_main`) VALUES
+('CasaHerley', 'Casa grande roja', 4, 3, 2, 1, 'hidden', 'chapinero', 250000, 8, 1, 1, ''),
+('CASA HERLEY', 'HDSBFHBDKJNBKJSDNAFJ', 5, 3, 2, 1, 'SI HAY JEJE', 'CHAPINERO', 250000, 8, 2, 1, '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `img` int(11) NOT NULL,
+  `id_house` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `mail` varchar(150) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `city` varchar(150) NOT NULL,
+  `gdpr` int(1) NOT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `mail`, `password`, `city`, `gdpr`, `role`) VALUES
+(1, 'Juan Carlos Ortiz', 'juan@gmail.com', '12345', 'Bogota', 1, 'Arrendatario'),
+(2, 'Sergio Sosa', 'sergio@gmail.com', 'T1NvTDl4Wlg4M2lUc3RBU3VPbDI0Zz09', 'Ibague', 0, 'Propietario');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `dates`
+--
+ALTER TABLE `dates`
+  ADD KEY `id_house` (`id_house`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indices de la tabla `houses`
+--
+ALTER TABLE `houses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `images`
+--
+ALTER TABLE `images`
+  ADD KEY `id_house` (`id_house`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `houses`
+--
+ALTER TABLE `houses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `dates`
+--
+ALTER TABLE `dates`
+  ADD CONSTRAINT `dates_ibfk_1` FOREIGN KEY (`id_house`) REFERENCES `houses` (`id`),
+  ADD CONSTRAINT `dates_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `houses`
+--
+ALTER TABLE `houses`
+  ADD CONSTRAINT `houses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`id_house`) REFERENCES `houses` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
